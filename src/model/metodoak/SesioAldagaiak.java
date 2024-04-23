cfcdf8273c378d234f3aa48ce1013f4095f6c437
in1dm3-d <in1dm3-d@elorrieta.int>
2024-04-23 08:51:32 +0200

    SQL Konexioan JAVADOC eginda

--- src/model/metodoak/SesioAldagaiak.java
+++ src/model/metodoak/SesioAldagaiak.java
@@ -3,6 +3,10 @@ package model.metodoak;
 import model.*;
 
 
+/**
+ * Klase honek SesioAldagaiak klasearen definizioa du.
+ * SesioAldagaiak klaseak erabiltzailearen egoera eta erabiltzaile mota gordetzen ditu.
+ */
 public class SesioAldagaiak {
 	public static E_Free erabiltzaile_free;
 	public static E_Premium erabiltzaile_premium;
